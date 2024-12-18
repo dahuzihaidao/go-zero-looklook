ba6d24f6d5157b86cd24783a9ea7d2ce47cfd016
dahuzi <dahuzihaidao@gmail.com>
2024-12-18 08:27:51 +0800

    modify gateway and gen.sh

--- deploy/script/gencode/gen.sh
+++ deploy/script/gencode/gen.sh
@@ -15,18 +15,11 @@
 #       go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway@latest
 #       go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2@latest
 #
-# 1）goctl >= 1.3 进入"服务/cmd/rpc/pb"目录下，执行下面命令
+# 进入"服务/cmd/rpc/pb"目录下，执行下面命令
 #    goctl rpc protoc *.proto --go_out=../ --go-grpc_out=../  --zrpc_out=../ --style=goZero
 #    去除proto中的json的omitempty
 #    mac: sed -i "" 's/,omitempty//g' *.pb.go
 #    linux: sed -i 's/,omitempty//g' *.pb.go
-# 2）goctl < 1.3 进入"服务/cmd"目录下，执行下面命令
-#    goctl rpc proto -src rpc/pb/*.proto -dir ./rpc --style=goZero
-#    去除proto中的json的omitempty
-#    mac: sed -i "" 's/,omitempty//g'  ./rpc/pb/*.pb.go
-#    linux: sed -i 's/,omitempty//g'  ./rpc/pb/*.pb.go
-
-
 
 # 创建kafka的topic
 # kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 -partitions 1 --topic {topic}
